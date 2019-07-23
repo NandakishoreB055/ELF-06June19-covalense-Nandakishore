431a6e06a4b7fa8e56453b6e8baa9aba981aa724
Nandakishore <nandakishore345.nk@gmail.com>
2019-07-23 10:05:13 +0530

    Springs assignment

---
+++ Assignment/Springs assignment/HardDisk.java
@@ -0,0 +1,38 @@
+package com.covalense.springcore.assignment;
+
+import lombok.extern.java.Log;
+
+@Log
+public class HardDisk implements StorageDevice {
+	void capcity() {
+		log.info("HardDisk capcity is 1TB ");
+	}
+
+	void writingSpeed() {
+		log.info("writingSpeed is 3.5 ");
+	}
+
+	void readingSpeed() {
+		log.info("readingSpeed is 3.5 ");
+
+	}
+
+	public void write() {
+		writingSpeed();
+		log.info("writing ");
+
+	}
+
+	public void read() {
+		readingSpeed();
+		log.info("reading ");
+
+	}
+
+	public void format() {
+		capcity();
+		log.info("formating ");
+
+	}
+
+}
---
+++ Assignment/Springs assignment/StorageDevice.java
@@ -0,0 +1,10 @@
+package com.covalense.springcore.assignment;
+
+public interface StorageDevice {
+	void write();
+
+	void read();
+
+	void format();
+
+}
