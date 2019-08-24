cae00f524675f188c07367c303a1abed2fbec0a6
Nandakishore <nandakishore345.nk@gmail.com>
2019-08-24 12:18:52 +0530

    library_management_system project

---
+++ library_management_system/lms-1/src/main/java/com/covalense/springboot/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.covalense.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.covalense.springboot.dto.UserRegisterBean;
+
+public interface UserRepository extends CrudRepository<UserRegisterBean, Integer> {
+
+	@Query("select u from UserRegisterBean u where u.email=:email")
+	public UserRegisterBean searchByEmail(@Param("email") String email);
+
+	@Query(value = "select * from user_register e where e.user_id in(select e.user_id from user_register e where e.user_id like  %:user_id%)", nativeQuery = true)
+	public List<UserRegisterBean> searchById(@Param("user_id") int user_id);
+}
